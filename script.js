24c81acd964f142276ac6ea02d676d1d83f49b3d
dudeka06 <eduardadina140@gmail.com>
2025-04-28 11:06:57 -0300

    hh

--- script.js
+++ script.js
@@ -33,4 +33,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 });
+const alternaContraste = document.getElementById('alterna-contraste')
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
 
